7ba807b03a0f6721d21ff60036100d664a7be44d
Md. Zubaer Rahman <zubaer.rahman.cse@gmail.com>
2023-06-18 16:55:59 +0600

    addVideo mutation added

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,9 +5,12 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:9000",
   }),
+  tagTypes: ["Videos"],
   endpoints: (builder) => ({
     getVideos: builder.query({
       query: () => "/videos",
+      keepUnusedDataFor: 600,
+      providesTags: ["Videos"]
     }),
     getVideo: builder.query({
       query: (id) => `/videos/${id}`,
@@ -17,11 +20,24 @@ export const apiSlice = createApi({
       query: (title) => {
         const tags = title.split(" ");
         const likes = tags.map((tag) => `title_like=${tag}`);
-        const queryString = `/videos?${likes.join("&")}&_limit=4`
+        const queryString = `/videos?${likes.join("&")}&_limit=4`;
         return queryString;
       },
+    }),
+    addVideo: builder.mutation({
+      query: (data) => ({
+        url: "/videos",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Videos"]
     }),
   }),
 });
 
-export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery} = apiSlice;
+export const {
+  useGetVideosQuery,
+  useGetVideoQuery,
+  useGetRelatedVideosQuery,
+  useAddVideoMutation,
+} = apiSlice;
